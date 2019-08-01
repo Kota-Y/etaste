7cd755a2a9e7bc2349d8ceab21c49fa1891e9980
Shuto <xabdjx@gmail.com>
2019-08-01 14:44:56 +0900

    create header[not linked]

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./Components/App";
 //import Body from './Components/Body';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 //ReactDOM.render(<Body />, document.getElementById('body'));
 
 // If you want your app to work offline and load faster, you can change
