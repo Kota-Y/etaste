1643f02032392ad0fb9df16e69cb2585dffe0c2d
kota-yamashita <mcz-0604-htt@outlook.com>
2019-08-30 03:31:46 +0900

    create store table and seed data

---
+++ api/src/database/migrations/2019_08_27_062623_create_stores_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',100);
+            $table->text('detail')->nullable();
+            $table->string('genre',50)->nullable();
+            $table->string('open_time',10);
+            $table->string('close_time',10);
+            $table->string('address',100);
+            $table->string('parking',50)->nullable();
+            $table->string('holiday',50)->nullable();
+            $table->string('store_url')->nullable();
+            $table->string('map',50);
+            $table->string('zip',10);
+            $table->string('access',100)->nullable();
+            $table->string('tel',20)->unique();
+            $table->string('image_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
