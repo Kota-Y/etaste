a0a5adf37b1aa548c58557a8f9fd206d162c979e
Shuto Nakano <xabdjx@gmail.com>
2019-09-03 22:07:51 +0900

    update google map

--- app/src/Components/StoreDetail.js
+++ app/src/Components/StoreDetail.js
@@ -28,75 +28,85 @@ class StoreDetail extends React.Component{
 
 /* Storeの情報をGETするメソッド(id1について) */
 componentDidMount() { 
-        axios
-            .get(　SuperKlass.CONST.DOMAIN + '/store/detail/1/'　,{
-                    headers: { "Content-Type": "application/json" },
-                    data: {}  
-                })
-            .then( (res) => {
-                this.setState({
-                    name: res.data.name,
-                    image: res.data.image,
-                    access: res.data.access,
-                    address: res.data.address,
-                    openTime: res.data.openTime,
-                    closeTime: res.data.closeTime,
-                    holiday: res.data.holiday,
-                    map: res.data.map,
-                    mapLatitude: res.data.mapLatitude,
-                    mapLongitude: res.data.mapLongitude,
-                    parking: res.data.parking,
-                    tel: res.data.tel,
-                    url: res.data.url,
-                    zip: res.data.zip
-                });
-                
-                //const mapLatitudeNum = Number(res.data.mapLatitude);
-                //const mapLongitudeNum = Number(res.data.mapLongitude);
+    axios
+        .get(　SuperKlass.CONST.DOMAIN + '/store/detail/1/', {
+                headers: { "Content-Type": "application/json" },
+                data: {}  
             })
-            .catch( (error) => {
-                console.log('通信に失敗しました');
+        .then( (res) => {
+            this.setState({
+                name: res.data.name,
+                image: res.data.image,
+                access: res.data.access,
+                address: res.data.address,
+                openTime: res.data.openTime,
+                closeTime: res.data.closeTime,
+                holiday: res.data.holiday,
+                map: res.data.map,
+                mapLatitude: res.data.mapLatitude,
+                mapLongitude: res.data.mapLongitude,
+                parking: res.data.parking,
+                tel: res.data.tel,
+                url: res.data.url,
+                zip: res.data.zip
             });
-    }
+            console.log(res.data);
+        })
+        .catch( (error) => {
+            console.log('通信に失敗しました');
+        });
+}
 
-    //const coordinates = { lat: Number(this.state.mapLatitude), lng: Number(this.state.mapLongitude) };
+/*　Storeの出品中の商品をGETするメソッド */
+
+
+
 
     render() {
         return(
             <div className='store-detail'>
-                <h1>店舗詳細</h1>
-                <div className='store-info'>
+                <div className='store-container'>
                     <div>
-                        <img
-                            src={this.state.image}
-                            alt=''
-                        />
+                        <h1>店舗詳細</h1>
                     </div>
-                    <div className='store-name'>
-                        <h2>{ this.state.name }</h2>
-                        <h3>{ this.state.openTime + '-' + this.state.closeTime}</h3>
-                        <h3>{ this.state.holiday }</h3>
-                        <h3>{ this.state.address }</h3>
-                        <h3>{ this.state.tel }</h3>
-                        <h3>
-                         <a href={this.state.url}>{ this.state.name + 'のHP' }</a>
-                        </h3>
+                    <div className='store-info'>
+                        <div>
+                            <img
+                                src={this.state.image}
+                                alt=''
+                            />
+                        </div>
+                        <div className='store-name'>
+                            <h2>{ this.state.name }</h2>
+                            <h3>{ this.state.openTime + '-' + this.state.closeTime}</h3>
+                            <h3>{ this.state.holiday }</h3>
+                            <h3>{ this.state.address }</h3>
+                            <h3>{ this.state.tel }</h3>
+                            <h3>
+                            <a href={this.state.url}>{ this.state.name + 'のHP' }</a>
+                            </h3>
+                        </div>
+                    </div>
+                    <div className='map'>
+                        <Map
+                        google={this.props.google}
+                        zoom={16}
+                        style={mapStyles}
+                        center={{ lat: Number(this.state.mapLatitude), lng: Number(this.state.mapLongitude)}}
+                        >
+                            <Marker 
+                                position={{ lat: Number(this.state.mapLatitude), lng: Number(this.state.mapLongitude)}}
+                                name={this.state.name}
+                            />
+                        </Map>
+                    </div> 
+                </div>
+                <div className='onSale-container'>
+                    <div><h1>出品中の商品</h1></div>
+                    <div className='onSale-items'>
+
                     </div>
                 </div>
-                <div className='map'>
-                    <Map
-                    google={this.props.google}
-                    zoom={16}
-                    style={mapStyles}
-                    center={{ lat: Number(this.state.mapLatitude), lng: Number(this.state.mapLongitude)}}
-                    >
-                        <Marker 
-                            position={{ lat: Number(this.state.mapLatitude), lng: Number(this.state.mapLongitude)}}
-                            name={this.state.name}
-                        />
-                    </Map>
-                </div>
-                
             </div>
         );
     }
