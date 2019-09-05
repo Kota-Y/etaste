0e9aa1491b0bc243d450c986430debc9431d1b00
Shuto Nakano <xabdjx@gmail.com>
2019-09-05 17:46:02 +0900

    assign Material-UI/Grid

--- app/src/Components/favoriteList.js
+++ app/src/Components/favoriteList.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { withRouter } from "react-router";
+import Grid from '@material-ui/core/Grid';
 //import axios from 'axios';
 
-class FavoriteLlist extends React.Component{
+import StoreComponent from './storeComponent';
+import '../CSS/favoriteList.css';
+
+class FavoriteList extends React.Component{
     render(){
         return(
-            <div>
-                <h1>お気に入り店舗の画面です</h1>
+            <div className='favList'>
+                <h1>お気に入り店舗</h1>
+                <div className='list-container'>
+                    <Grid container>
+                        <Grid item xs={3}><StoreComponent /></Grid>
+                        <Grid item xs={3}><StoreComponent /></Grid>
+                        <Grid item xs={3}><StoreComponent /></Grid>
+                        <Grid item xs={3}><StoreComponent /></Grid>
+                        <Grid item xs={3}><StoreComponent /></Grid>
+                    </Grid>
+                </div>
             </div>
-            );
+        );
     }
 }
 
-export default withRouter(FavoriteLlist);
+export default withRouter(FavoriteList);
