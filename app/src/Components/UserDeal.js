215dd6664a9ca49d44310d94817139a0a83a6d5e
Shuto Nakano <xabdjx@gmail.com>
2019-09-12 16:29:02 +0900

    1st update user deal page

---
+++ app/src/Components/UserDeal.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { withRouter } from "react-router";
+
+class UserDeal extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <h1>受け取り予定・履歴画面</h1>
+      </div>
+    );
+  }
+}
+
+export default withRouter(UserDeal);
