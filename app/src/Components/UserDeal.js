9740f62b5e15c4b46f145a783a4a2a23ecf7a5e5
Shuto Nakano <xabdjx@gmail.com>
2019-09-12 18:14:03 +0900

    Split screen

--- app/src/Components/UserDeal.js
+++ app/src/Components/UserDeal.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { withRouter } from "react-router";
 
+import "../CSS/Deal.css";
+
 class UserDeal extends React.Component {
   
   render() {
     return (
-      <div>
-        <h1>受け取り予定・履歴画面</h1>
-      </div>
+        <div className='main'>
+            <div className='book-container'>
+                <h1>受け取り予定</h1>
+            </div>
+            <div className='tradingHistory-container'>
+                <h1>取引履歴</h1>
+            </div>
+         </div>
     );
   }
 }
