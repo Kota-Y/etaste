907754b35aaed016b8c456d157569658a64be41b
Gkuma <koutag0317@gmail.com>
2019-09-16 14:38:09 +0900

    syuppin layout

--- app/src/Components/Store-Input.js
+++ app/src/Components/Store-Input.js
@@ -23,7 +23,9 @@ class StoreInput extends React.Component {
       itemName: '',
       itemNumber:'',
       startTime:'',
-      endTime:''
+      endTime:'',
+      originalprice:'',
+      saleprice:''
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleToStoreSyuppinPage = this.handleToStoreSyuppinPage.bind(this)
@@ -35,20 +37,17 @@ class StoreInput extends React.Component {
       state:{itemName: this.state.itemName,
              itemNumber: this.state.itemNumber,
              startTime: this.state.startTime,
-             endTime: this.state.endTime
+             endTime: this.state.endTime,
+             originalprice: this.state.originalprice,
+             saleprice: this.state.saleprice
               }
     });
 
   };
 
-  handleChange(e){
+  handleChange(e){ //それぞれの要素のname属性に対応した変数にvalueを格納
     this.setState({
       [e.target.name]: e.target.value
-    })
-  }
-  handleChange2(e){    //select用
-    this.setState({
-      [e.target.name]: e.target.options
     })
   }
 
@@ -58,16 +57,21 @@ class StoreInput extends React.Component {
       <div>
       <h1>店舗側入力</h1>
         <div className="box-store-input">
-         <div>
+          <div className='item-image'>
             <h2>商品画像</h2>
           </div>
           
           <div className="item-name">
-            <input type='text' value={this.state.itemName}　name='itemName' onChange={this.handleChange} placeholder="商品名"/>
+            <input type='text' 
+            value={this.state.itemName}　
+            name='itemName' 
+            onChange={this.handleChange} 
+            placeholder="商品名"/>
           </div>
           
-          <div className="receipt-time">
+          <div className="startTime">
             <Select
+              className='select'
               options={times}
               styles={{
                 [styleKeys]: styleFn
@@ -75,12 +79,13 @@ class StoreInput extends React.Component {
               value={this.state.startTime}
               placeholder='受取開始時間'
               name='startTime'
-              onChange={this.handleChange2}
+              onChange={this.handleChange}
             />
           </div>
 
-          <div className="receipt-time">
+          <div className="endTime">
             <Select
+              className='select'
               options={times}
               styles={{
                 [styleKeys]: styleFn
@@ -92,28 +97,39 @@ class StoreInput extends React.Component {
             />
           </div>
           
-          <div className="originlprice">
-            <input placeholder="定価(半角数字)"/>
+          <div className="originalprice">
+            <input 
+            value={this.state.originalprice}　
+            name='originalprice' 
+            onChange={this.handleChange} 
+            placeholder="定価(半角数字)"/>
             <h4 className='en'>円</h4>
           </div>
 
           <div className="saleprice">
-            <input placeholder="販売価格(半角数字)"/>
+            <input 
+            value={this.state.saleprice}　
+            name='saleprice' 
+            onChange={this.handleChange} 
+            placeholder="販売価格(半角数字)"/>
             <h4 className='en'>円</h4>
           </div>
 
         
-          <div className="item-number">
-            <input value={this.state.itemNumber}　name='itemNumber' onChange={this.handleChange} placeholder="個数(半角数字)"/>
+          <div className="itemnumber">
+            <input value={this.state.itemNumber}　
+            name='itemNumber' 
+            onChange={this.handleChange} 
+            placeholder="個数(半角数字)"/>
+            <h4 className='ko'>個</h4>
           </div>
 
           <div className="">
             <h3>アレルギー表示</h3>
 
-            <input placeholder="アレルギー"/>
           </div>
-
-          <input onClick={this.handleToStoreSyuppinPage} type="submit" value="出品確認"/>
+          <button className='storesubmit' onClick={this.handleToStoreSyuppinPage}>出品確認</button>
+          {/*<input onClick={this.handleToStoreSyuppinPage} type="submit" value="出品確認"/>*/}
         </div>
       </div>
     );
--- app/src/Components/Store-Syuppin.js
+++ app/src/Components/Store-Syuppin.js
@@ -1,17 +1,27 @@
 import { withRouter } from "react-router";
 import React,{Component} from "react";
-
+import "../CSS/Store-Syuppin.css";
 
 class StoreSyuppin extends React.Component {
   
+  handleToStorefinPage = () => {
+    this.props.history.push("/store-fin");
+  };
+  
   render() {
     return (
-      <div>
-        <h1>出品画面です</h1>
-        <p>{this.props.location.state.itemName}</p>
-        <p>{this.props.location.state.itemNumber}</p>
-        <p>{this.props.location.state.startTime}</p>
-        <p>{this.props.location.state.endTime}</p>
+      <div className='main'>
+
+        <div className='check-area'>
+          <p>購入商品：{this.props.location.state.itemName}</p>
+          <p>購入個数：{this.props.location.state.itemNumber}</p>
+          <p>受け取り開始時間：{this.props.location.state.startTime}</p>
+          <p>受け取り終了時間：{this.props.location.state.endTime}</p>
+          <p>定価：{this.props.location.state.originalprice}円</p>
+          <p>販売価格：{this.props.location.state.saleprice}円</p>
+          <p>アレルギー：</p>
+        </div>
+        <button className='storesubmit' onClick={this.handleToStorefinPage}>確認完了</button>
       </div>
     );
   }
