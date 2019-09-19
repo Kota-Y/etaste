de05ab9f8a9ee0947aed57db739cc5e06889f2d4
Shuto Nakano <xabdjx@gmail.com>
2019-09-19 19:16:54 +0900

    Create Login Page (layout)

--- app/src/Components/Login.js
+++ app/src/Components/Login.js
@@ -2,22 +2,36 @@ import React from "react";
 import { withRouter } from "react-router";
 import { FacebookLoginButton, TwitterLoginButton, InstagramLoginButton } from "react-social-login-buttons";
 
+import '../CSS/Login.css';
+
 class Login extends React.Component {
   
   render() {
     return (
-      <div>
-        <h1>ログイン画面</h1>
-        <FacebookLoginButton onClick={() => alert("Hello")}>
-            <span>Custom text</span>
-        </FacebookLoginButton>
-        <TwitterLoginButton onClick={() => alert("Hello")}>
-            <span>Custom text</span>
-        </TwitterLoginButton>
-        <InstagramLoginButton onClick={() => alert("Hello")}>
-            <span>Custom text</span>
-        </InstagramLoginButton>
-      </div>
+        <div className='login-page'>
+            <div className='social-login-button'>
+                <FacebookLoginButton size='45px'>
+                    <span>Facebookでログイン</span>
+                </FacebookLoginButton >
+                <TwitterLoginButton size='45px'>
+                    <span>Twitterでログイン</span>
+                </TwitterLoginButton>
+                <InstagramLoginButton size='45px'>
+                    <span>Instagramでログイン</span>
+                </InstagramLoginButton>
+            </div>
+            <form>
+                <div className='input-form'>
+                    <input type="email" name="mailAdress" value="メールアドレス" />
+                </div>
+                <div className='input-form'>
+                    <input type="text" name="password" value="パスワード" />
+                </div>
+            </form>
+            <h2>ログイン</h2>
+            <h3>パスワードを忘れた方</h3>
+            <h3>アカウント新規作成</h3>
+        </div>
     );
   }
 }
