bf6f871223b187a798e59ef5b7b1db795c914dac
Shuto Nakano <xabdjx@gmail.com>
2019-09-25 14:09:29 +0900

    update validation check Login-form

--- app/src/Components/ValidatedLoginForm.js
+++ app/src/Components/ValidatedLoginForm.js
@@ -37,13 +37,13 @@ const ValidatedLoginForm = () => (
     //********Using Yum for validation********/
 
     validationSchema={Yup.object().shape({
-      email: Yup.string()
-        .email()
-        .required("Required"),
-      password: Yup.string()
-        .required("No password provided.")
-        .min(8, "Password is too short - should be 8 chars minimum.")
-        .matches(/(?=.*[0-9])/, "Password must contain a number.")
+        email: Yup.string()
+            .email()
+            .required("Required"),
+        password: Yup.string()
+            .required("No password provided.")
+            .min(8, "Password is too short - should be 8 chars minimum.")
+            .matches(/(?=.*[0-9])/, "Password must contain a number.")
     })}
   >
     {props => {
