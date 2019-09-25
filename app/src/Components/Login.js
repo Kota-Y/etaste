5e48b6889907bf8a0855e7ca0fdc8b9b6c8470d0
Shuto Nakano <xabdjx@gmail.com>
2019-09-25 14:16:10 +0900

    2nd update validation check Login-form

--- app/src/Components/Login.js
+++ app/src/Components/Login.js
@@ -6,20 +6,12 @@ import '../CSS/LoginFeature.css'
 import ValidatedLoginForm from "./ValidatedLoginForm";
 
 class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          email: "",
-          password: ""
-        };
-    }
 
     handleToSignUpPage = () => {
         this.props.history.push("/sign-up");
     };
   
     render() {
-        //const { email, password } = this.state;
 
         return (
             <div className='login-page'>
@@ -34,37 +26,7 @@ class Login extends React.Component {
                         <span>Instagramでログイン</span>
                     </InstagramLoginButton>
                 </div>
-                {/* <form onSubmit={this.handleSubmit}　className='input-form'>
-                    <label htmlFor="email">Email</label>
-                    <input
-                    name="email"
-                    type="text"
-                    placeholder="Enter your email"
-                    value={email}
-                    onChange={this.handleChange}
-                    />
-                    <label htmlFor="email">Password</label>
-                    <input
-                    name="password"
-                    type="password"
-                    placeholder="Enter your password"
-                    value={password}
-                    onChange={this.handleChange}
-                    />
-                    <h2>Login</h2>
-                </form> */}
                 <ValidatedLoginForm />
-                {/* <form>
-                    <div className='input-form'>
-                        <label>メールアドレス</label>
-                        <input type="email" name="mailAdress" />
-                    </div>
-                    <div className='input-form'>
-                        <label>パスワード</label>
-                        <input type="password" name="password" />
-                    </div>
-                </form> 
-                <h2>ログイン</h2>*/}
                 <h3>パスワードを忘れた方</h3>
                 <h3
                     onClick={ this.handleToSignUpPage }
