93736703f710d5b3d89d3a8cc28d8fbcf76f9e72
Shuto Nakano <xabdjx@gmail.com>
2019-09-27 10:41:32 +0900

    add link to mailCheck-Page

--- app/src/Components/SignUp.js
+++ app/src/Components/SignUp.js
@@ -5,12 +5,30 @@ import "../CSS/LoginFeature.css";
 import ValidatedSignUpForm from "./ValidatedSignUpForm";
 
 class SignUp extends React.Component {
+    /* handleToMailCheckPage = () => {
+        this.props.history.push("/mail-check");
+    }; */
     render() {
         return (
         <div className='signup-container'>
-            <div>
-                <ValidatedSignUpForm />      
-            </div>
+            {/* <form>
+                <div>
+                    <p class="req">氏名</p>
+                    <input type="text" placeholder="氏名" name="text" required autoForcus />
+                </div>
+                <div>
+                    <p class="req">メールアドレス</p>
+                    <input type="email" placeholder="メールアドレス" name="email"
+                            pattern="[\w\d_-]+@[\w\d_-]+\.[\w\d._-]+" required />
+                </div>
+                <div>
+                    <p>電話番号</p>
+                    <input type="tel" placeholder="電話番号" name="tel"
+                            pattern="^\d{11}$|^\d{3,4}-\d{3,4}-\d{4}$"/>
+                </div>
+                <button type="submit">送信する</button>
+            </form> */}
+            <ValidatedSignUpForm />  
         </div>
     );
   }
--- app/src/function/DefineConst.js
+++ app/src/function/DefineConst.js
@@ -5,16 +5,6 @@ class SuperKlass {
         });
     }
 }
-
-/*class SubKlass extends SuperKlass {
-    static get CONST() {
-        return Object.assign(super.CONST || {}, {
-            PIYO : 4,
-            PON : 5
-        });
-    }
-}*/
-
 export default SuperKlass;
 
 /* ストアとユーザーの判別をする(仮) */
@@ -22,3 +12,11 @@ export const isStore = false;
 
 /* ユーザーのログイン状態の判断(仮) */
 export const isLogin =　false;
+
+export const hoge = () => {
+    if(document.getElementById('check').checked === true) {
+      document.getElementById('but').disabled = false;
+    } else {
+      document.getElementById('but').disabled = true;
+    }
+  }
