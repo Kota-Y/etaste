1b3a95b18d72461bc1b51653c1a7ddb0b025bda1
kota-yamashita <mcz-0604-htt@outlook.com>
2019-10-01 17:43:12 +0900

    add createuser

--- api/src/app/Http/Controllers/Api/UsersController.php
+++ api/src/app/Http/Controllers/Api/UsersController.php
@@ -10,7 +10,27 @@ class UsersController extends Controller
 {
     public function store(Request $request)
     {
-        //
+        $md = new User();
+
+        $md->first_name = $request->input('firstName');
+        $md->last_name = $request->input('lastName');
+        $md->first_name_kana = $request->input('firstNameKana');
+        $md->last_name_kana = $request->input('lastNameKana');
+        $md->mail = $request->input('mail');
+        $md->password = bcrypt($request->input('password'));
+        $md->is_login = false;
+        $md->is_store = false;
+
+        $md->save();
+
+        $data_json = [];
+
+        return response()->json(
+            $data_json,
+            201,
+            [],
+            JSON_UNESCAPED_UNICODE
+        );
     }
 
     public function login()
